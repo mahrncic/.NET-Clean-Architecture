52cd7ab3ebf3675c6793e1975ebea71c5d1debba
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-10 23:32:44 +0200

    Added Get Leave Type Detail Request with Handler and MediatR package.

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeDetailRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.DTOs;
+using LeaveManagement.Application.Features.LeaveTypes.Requests;
+using LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeDetailRequestHandler : IRequestHandler<GetLeaveTypeDetailRequest, LeaveTypeDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeDetailRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<LeaveTypeDto> Handle(GetLeaveTypeDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            return _mapper.Map<LeaveTypeDto>(leaveType);
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Requests/GetLeaveTypeDetailRequest.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeDetailRequest : IRequest<LeaveTypeDto>
+    {
+        public int Id { get; set; }
+    }
+}
