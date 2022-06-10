71771151129da816268335b60bc17aede48d5ce9
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-10 23:29:28 +0200

    Added GetTypeLeave List request with Handler using the MediatR package and CQRS pattern.

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Requests/GetLeaveTypeListRequest.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagement.Application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
+    {
+
+    }
+}
