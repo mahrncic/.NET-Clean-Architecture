7bd8537e1c26c6102f5409d8d550516b09892054
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-10 23:19:43 +0200

    Added MediatR package into the Application layer of the solution.

--- LeaveManagement/LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ LeaveManagement/LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace LeaveManagement.Application
 {
     public static class ApplicationServicesRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
         }
     }
 }
