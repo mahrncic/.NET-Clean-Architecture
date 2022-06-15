0e832703eeeb2ba483a2b614c76d838c2b450e26
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-15 21:14:22 +0200

    Added new DTOs into the Leave Management Application layer for Leave Allocation, Request and Type entities.

--- LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeCommand.cs
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeCommand.cs
@@ -5,6 +5,6 @@ namespace LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
 {
     public class CreateLeaveTypeCommand : IRequest<int>
     {
-        public LeaveTypeDto LeaveTypeDto { get; set; }
+        public CreateLeaveTypeDto LeaveTypeDto { get; set; }
     }
 }
