70b560965ef776bde90ade90df71777df9720e4c
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-15 23:01:36 +0200

    Added custom response to be returned on the Create Leave Request command handler.

--- LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -2,16 +2,16 @@
 using LeaveManagement.Application.DTOs.LeaveRequest.Validators;
 using LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
 using LeaveManagement.Application.Persistence.Contracts;
+using LeaveManagement.Application.Responses;
 using LeaveManagement.Domain;
 using MediatR;
-using System;
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
 {
-    public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, int>
+    public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, BaseCommandResponse>
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly ILeaveTypeRepository _leaveTypeRepository;
@@ -24,21 +24,28 @@ namespace LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
             _leaveTypeRepository = leaveTypeRepository;
         }
 
-        public async Task<int> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+        public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
         {
+            var response = new BaseCommandResponse();
             var validator = new CreateLeaveRequestDtoValidator(_leaveTypeRepository);
             var validationResult = await validator.ValidateAsync(request.LeaveRequestDto);
 
             if (validationResult.IsValid == false)
             {
-                throw new ValidationException(validationResult);
+                response.Success = false;
+                response.Message = "Creation Failed";
+                response.Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
             }
 
             var leaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
 
             leaveRequest = await _leaveRequestRepository.Add(leaveRequest);
 
-            return leaveRequest.Id;
+            response.Success = true;
+            response.Message = "Creation Successful";
+            response.Id = leaveRequest.Id;
+
+            return response;
         }
     }
 }
