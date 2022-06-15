3e805fdc82ea2f00b884ae19fa45ed0a12e3d8ab
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-15 22:05:22 +0200

    Implemented validators with fluent validation for the Create LeaveType/Leave Allocation requests.

---
+++ LeaveManagement/LeaveManagement.Application/DTOs/LeaveAllocation/Validators/CreateLeaveAllocatiorDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using LeaveManagement.Application.Persistence.Contracts;
+
+namespace LeaveManagement.Application.DTOs.LeaveAllocation.Validators
+{
+    public class CreateLeaveAllocatiorDtoValidator : AbstractValidator<CreateLeaveAllocationDto>
+    {
+        public CreateLeaveAllocatiorDtoValidator(ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            RuleFor(p => p.NumberOfDays)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0)
+                .LessThan(100);
+
+            RuleFor(p => p.Period)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0)
+                .LessThan(100);
+
+            RuleFor(p => p.LeaveTypeId)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0)
+                .MustAsync(async (leaveTypeId, cancellation) =>
+                {
+                    var leaveType = await leaveAllocationRepository.Get(leaveTypeId);
+                    return leaveType != null;
+                }).WithMessage("Invalid ID is provided for the Leave Type foreign key."); ;
+        }
+    }
+}
