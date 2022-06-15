c06ac7b91e8ed564c5beabd91aaf7a2c1a72cdeb
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-15 21:44:29 +0200

    Implemented Delete functionality Commands and Handlers for Allocation, Types and Requests.

---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveAllocations/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+using LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Features.LeaveAllocations.Handlers.Commands
+{
+    public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+        public DeleteLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+        }
+
+
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.Get(request.Id);
+
+            await _leaveAllocationRepository.Delete(leaveAllocation);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+
+            await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
