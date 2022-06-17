22e8e3d4c9623508f829e75329ddf28744e81e76
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-17 23:03:51 +0200

    Added Db Context class with DB Sets based on the domain objects.

---
+++ LeaveManagement/Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
