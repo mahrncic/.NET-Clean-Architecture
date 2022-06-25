3a866e23e6f0e4f171b85f343dc8aeef21d34551
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-25 23:09:40 +0200

    Added LeaveType repository into the Persistence project in the Infrastructure layer.

---
+++ LeaveManagement/Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagement.Application.Persistence.Contracts;
+using LeaveManagement.Domain;
+
+namespace Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        public LeaveTypeRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
