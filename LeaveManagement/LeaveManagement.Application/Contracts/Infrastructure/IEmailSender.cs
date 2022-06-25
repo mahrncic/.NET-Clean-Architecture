9a5eb086853441d83458b9e3f101082d630d2837
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-25 23:32:54 +0200

    Added EmailSender interface with an email model in the Core layer of the solution (Application project).

---
+++ LeaveManagement/LeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagement.Application.Models;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
