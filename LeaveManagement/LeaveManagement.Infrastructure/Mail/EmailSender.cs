b6a0e6e03dac08a984695008519bac310d2fa21b
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-25 23:46:21 +0200

    Added an EmailSender implementation for sending emails with SendGrid in the Infrastructure layer.

---
+++ LeaveManagement/LeaveManagement.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using LeaveManagement.Application.Contracts.Infrastructure;
+using LeaveManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private EmailSettings _settings { get; }
+
+        public EmailSender(IOptions<EmailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_settings.ApiKey);
+
+            var to = new EmailAddress(email.To);
+            var from = new EmailAddress
+            {
+                Email = _settings.FromAddress,
+                Name = _settings.FromName
+            };
+
+            var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+
+            var response = await client.SendEmailAsync(message);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
