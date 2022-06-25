64c39d32511551885d0448bc1bcca855c9ce40d2
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-25 23:24:19 +0200

    Added PersistenceServicesRegistration for DI in the Infrastructure Layer.

---
+++ LeaveManagement/Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,26 @@
+﻿using LeaveManagement.Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+
+namespace Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
