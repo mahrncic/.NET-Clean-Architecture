87b820611210cc16a7748592128c78908012f710
Marko Hrncic <marko.hrncic@kogitas.com>
2022-06-25 23:13:43 +0200

    Added implementation for LeaveRequest repository in the Infrastructure layer.

--- LeaveManagement/Persistence/Repositories/LeaveTypeRepository.cs
+++ LeaveManagement/Persistence/Repositories/LeaveTypeRepository.cs
@@ -5,8 +5,11 @@ namespace Persistence.Repositories
 {
     public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
     {
+        private readonly LeaveManagementDbContext _dbContext;
+
         public LeaveTypeRepository(LeaveManagementDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
         }
     }
 }
